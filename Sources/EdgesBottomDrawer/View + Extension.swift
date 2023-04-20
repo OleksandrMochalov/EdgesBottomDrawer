6b5ea3bb4ee8df6c4aecf66e3af30671ce86390b
AlexMochalov <oleksandr.mochalov@monstar-lab.com>
2023-04-20 22:26:53 +0200

    feat: bottom drawer view

---
+++ Sources/EdgesBottomDrawer/View + Extension.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by AlexMochalov on 20.04.2023.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func bottomDrawerView<DrawerContent: View, PullUpView: View>(
+        bottomDrawerHeight: CGFloat = 100,
+        @ViewBuilder drawerContent: @escaping () -> DrawerContent,
+        @ViewBuilder pullUpView: @escaping (_ shouldGoUp: Bool) -> PullUpView
+    ) -> some View {
+        self.modifier(BottomDrawerModifier(
+            bottomDrawerHeight: bottomDrawerHeight,
+            drawerContent: drawerContent,
+            pullUpView: pullUpView
+        ))
+    }
+}
